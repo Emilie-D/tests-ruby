a964b0af66170ef4153e119dbf95f84e16179913
Emilie-D <emyss42016@gmail.com>
2019-04-08 19:31:08 +0200

    terminé

---
+++ lib/01_temperature.rb
@@ -0,0 +1,20 @@
+def ftoc(fahrenheit)
+	resultat = (fahrenheit.to_f - 32) * 5/9
+	return resultat
+	#(32 °F − 32) × 5/9 = 0 °C
+end
+
+def ctof(celsius)
+	resultat = (celsius.to_f * 9/5) + 32
+	return resultat
+	#(0 °C × 9/5) + 32 = 32 °F
+end 
+
+#ftoc(32)
+#ftoc(212)
+#ftoc(98.6)
+#ftoc(68)
+#ctof(0)
+#ctof(100)
+#ctof(20)
+#ctof(37)
